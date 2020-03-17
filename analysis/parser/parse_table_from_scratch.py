f3d9c04d63e57cfee0959b1990603161bd9b2a82
Julaiti Alafate <jalafate@gmail.com>
2020-03-17 00:36:01 -0700

    format

--- analysis/parser/parse_table_from_scratch.py
+++ analysis/parser/parse_table_from_scratch.py
@@ -1,19 +1,19 @@
 import sys
 
-num_rows = 8
+num_rows = 7
 num_cols = 8
 
 with open(sys.argv[1]) as f:
     for row in range(num_rows):
-        if row == num_rows - 1:
-            print("\\hline")
+        #if row == num_rows - 1:
+        #    print("\\hline")
         for col in range(num_cols):
             line = f.readline()
             vals = line.split()
             row_name = vals[0].strip()
             value = float(vals[-1])
             if col == 0:
-                print("{} ".format(row_name), end='')
+                print("{} ".format(row_name.replace("_", " ")), end='')
             if col == row:
                 print("& {\color{blue} %.2f } " % (value * 100.0), end='')
             else:
