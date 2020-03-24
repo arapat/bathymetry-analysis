efadcd3f79cda538fc15fc46271a3640abc1323a
Julaiti Alafate <jalafate@gmail.com>
2020-03-24 00:25:06 -0700

    updated parser

--- analysis/parser/parse_table.py
+++ analysis/parser/parse_table.py
@@ -2,11 +2,15 @@ import sys
 
 
 with open(sys.argv[1]) as f:
-    for line in f:
-        for eg in line.split():
+    for i, line in enumerate(f):
+        for j, eg in enumerate(line.split(',')):
             try:
                 t = float(eg)
-                print("%.2f &" % (t * 100.0), end=' ')
+                val = "%.2f" % (t * 100.0)
+                if i + 1 == j:
+                    print("{ \\color{blue} \\textbf{" + val + "} } &", end=' ')
+                else:
+                    print(val + " &", end=' ')
             except:
                 print(eg.strip() + " &", end=' ')
         print('\b\b\\\\')
